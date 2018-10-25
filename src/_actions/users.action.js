7eecd321a759ebaf812de87449ad9694b32e83a2
clkent <chelsea.kent@gmail.com>
2018-10-24 18:53:21 -0700

    updating for signup form validation

--- src/_actions/users.action.js
+++ src/_actions/users.action.js
@@ -14,6 +14,27 @@ export const signUpUser = user => dispatch => {
     .then(res => normalizeResponseErrors(res))
     .then(res => res.json())
     .catch(err => {
+      if (
+        err.message == 'A user with that username and/or email already exists.'
+      ) {
+        const { message } = err;
+        return Promise.reject(
+          new SubmissionError({
+            username: message
+          })
+        );
+      }
+
+      if (err.error.isJoi) {
+        const joiMessage = err.error.details[0].message;
+        const joiLocation = err.error.details[0].path[0];
+        return Promise.reject(
+          new SubmissionError({
+            [joiLocation]: joiMessage
+          })
+        );
+      }
+
       const { message } = err;
       // Convert ValidationErrors into SubmissionErrors for Redux Form
       return Promise.reject(
